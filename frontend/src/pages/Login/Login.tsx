b1739ef5afa05c51712afcc072b88ebb7644b291
DongRyeong Lee <dr11291129@gmail.com>
2023-11-15 15:38:17 +0900

    feat: login page ui 프로토타입 작업 #7
    로그인 페이지의 기본적인 ui 작업을 완료하였습니다.
    
    Co-authored-by: Jw705 <sirius_75@naver.com>
    Co-authored-by: Byeonjin <quswlstkd88@naver.com>

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import LoginSection from "./components/LoginSection";
 
 const Login = () => {
   return (
     <>
       <Header />
+      <div className="flex flex-1 items-center justify-center h-screen">
+        <LoginSection />
+      </div>
     </>
   );
 };
--- frontend/src/pages/Login/components/Header.tsx
+++ frontend/src/pages/Login/components/Header.tsx
@@ -2,10 +2,10 @@ import logoSmall from "@/assets/imgs/logoSmall.png";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between px-6 py-4 bg-white border-header">
+    <header className="flex w-screen items-center justify-between px-6 py-4 bg-white border-header box-border">
       <button type="button" className="flex flex-row items-center gap-4 semibold-20">
         <img src={logoSmall} />
-        Boarlog
+        <h1>Boarlog</h1>
       </button>
       <button type="button" className="medium-16">
         Boarlog 체험하기
