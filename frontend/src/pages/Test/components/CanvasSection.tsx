6f51e48d5ff9cfe7935a724b3281da24f627ed8f
Juwan <sirius_75@naver.com>
2023-11-19 21:29:38 +0900

    refactor: 툴바를 component로 분리 #50
    툴바를 Toolbar Component로 분리했습니다.

--- frontend/src/pages/Test/components/CanvasSection.tsx
+++ frontend/src/pages/Test/components/CanvasSection.tsx
@@ -1,11 +1,7 @@
 import { fabric } from "fabric";
 import { useEffect, useRef, useState } from "react";
 
-import MouseIcon from "@/assets/svgs/whiteboard/mouse.svg?react";
-import PenIcon from "@/assets/svgs/whiteboard/pen.svg?react";
-import StickyNoteIcon from "@/assets/svgs/whiteboard/stickyNote.svg?react";
-import ImageIcon from "@/assets/svgs/whiteboard/image.svg?react";
-import EraserIcon from "@/assets/svgs/whiteboard/eraser.svg?react";
+import Toolbar from "./Toolbar";
 
 const CanvasSection = () => {
   const canvasContainerRef = useRef<HTMLDivElement>(null);
@@ -120,33 +116,13 @@ const CanvasSection = () => {
     <div className="relative w-[100vw] h-[calc(100vh-6rem)]" ref={canvasContainerRef}>
       <canvas className="" ref={canvasRef} />
 
-      <div className="flex flex-col items-center justify-center p-2 gap-1 rounded-[10px] bg-grayscale-lightgray border border-grayscale-lightgray shadow-md absolute top-2.5 left-2.5">
-        <button
-          className="flex p-2 items-center justify-center rounded-[10px] disabled:bg-boarlog-80 group"
-          onClick={setSelectMode}
-          disabled={activeTool === "select"}
-          title="Select Tool"
-        >
-          <MouseIcon className="group-disabled:fill-white" />
-        </button>
-        <button
-          className="flex p-2 items-center justify-center rounded-[10px] disabled:bg-boarlog-80 group"
-          onClick={setPenMode}
-          disabled={activeTool === "pen"}
-          title="Pen Tool"
-        >
-          <PenIcon className="group-disabled:fill-white" />
-        </button>
-        <button className="flex p-2 items-center justify-center rounded-[10px]" onClick={addRectangle}>
-          <StickyNoteIcon />
-        </button>
-        <button className="flex p-2 items-center justify-center rounded-[10px]" onClick={erase}>
-          <ImageIcon />
-        </button>
-        <button className="flex p-2 items-center justify-center rounded-[10px]" onClick={erase}>
-          <EraserIcon />
-        </button>
-      </div>
+      <Toolbar
+        activeTool={activeTool}
+        setSelectMode={setSelectMode}
+        setPenMode={setPenMode}
+        addRectangle={addRectangle}
+        erase={erase}
+      />
     </div>
   );
 };
---
+++ frontend/src/pages/Test/components/Toolbar.tsx
@@ -0,0 +1,47 @@
+import MouseIcon from "@/assets/svgs/whiteboard/mouse.svg?react";
+import PenIcon from "@/assets/svgs/whiteboard/pen.svg?react";
+import StickyNoteIcon from "@/assets/svgs/whiteboard/stickyNote.svg?react";
+import ImageIcon from "@/assets/svgs/whiteboard/image.svg?react";
+import EraserIcon from "@/assets/svgs/whiteboard/eraser.svg?react";
+
+interface ToolbarProps {
+  activeTool: string;
+  setSelectMode: () => void;
+  setPenMode: () => void;
+  addRectangle: () => void;
+  erase: () => void;
+}
+
+const Toolbar = ({ activeTool, setSelectMode, setPenMode, addRectangle, erase }: ToolbarProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center p-2 gap-1 rounded-[10px] bg-grayscale-lightgray border border-grayscale-lightgray shadow-md absolute top-2.5 left-2.5">
+      <button
+        className="flex p-2 items-center justify-center rounded-[10px] disabled:bg-boarlog-80 group"
+        onClick={setSelectMode}
+        disabled={activeTool === "select"}
+        title="Select Tool"
+      >
+        <MouseIcon className="group-disabled:fill-white" />
+      </button>
+      <button
+        className="flex p-2 items-center justify-center rounded-[10px] disabled:bg-boarlog-80 group"
+        onClick={setPenMode}
+        disabled={activeTool === "pen"}
+        title="Pen Tool"
+      >
+        <PenIcon className="group-disabled:fill-white" />
+      </button>
+      <button className="flex p-2 items-center justify-center rounded-[10px]" onClick={addRectangle}>
+        <StickyNoteIcon />
+      </button>
+      <button className="flex p-2 items-center justify-center rounded-[10px]" onClick={erase}>
+        <ImageIcon />
+      </button>
+      <button className="flex p-2 items-center justify-center rounded-[10px]" onClick={erase}>
+        <EraserIcon />
+      </button>
+    </div>
+  );
+};
+
+export default Toolbar;
