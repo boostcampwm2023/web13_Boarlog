c6f2f641d461bdab6a483d5f326738a9fca93d36
DongRyeong Lee <dr11291129@gmail.com>
2023-11-20 20:40:22 +0900

    feat: 강의자 Header button UI 작업 #62
    - SmallButton 공용 component를 작성하였습니다.
    - 작성한 component를 바탕으로 강의자 header button을 구현하였습니다.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,17 +5,24 @@ import RightSection from "./components/RightSection";
 
 interface HeaderProps {
   lecture?: boolean;
+  lecturer?: boolean;
   login?: boolean;
   main?: boolean;
 }
 
-const Header = ({ lecture, login, main }: HeaderProps) => {
+const Header = ({ lecture, lecturer, login, main }: HeaderProps) => {
   const [profileClicked, setProfileClicked] = useState(false);
 
   return (
     <header className="flex w-100 relative items-center justify-between px-6 py-4 bg-white border-header box-border">
       <LeftSection lecture={lecture} />
-      <RightSection profileClicked={profileClicked} setProfileClicked={setProfileClicked} login={login} main={main} />
+      <RightSection
+        profileClicked={profileClicked}
+        setProfileClicked={setProfileClicked}
+        lecturer={lecturer}
+        login={login}
+        main={main}
+      />
       <ProfileModal profileClicked={profileClicked} setProfileClicked={setProfileClicked} />
     </header>
   );
