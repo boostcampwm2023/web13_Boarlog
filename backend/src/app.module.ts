5489f4667bd3486acad2d253623fa7358b92ea7a
tmddus2 <tmddus2__@naver.com>
2023-11-21 10:56:26 +0900

    feat: mongoDB 연결

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration]
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
