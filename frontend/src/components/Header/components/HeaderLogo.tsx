ac1e17e65a4eee752fc9bb0ed0c4cca141440fd3
DongRyeong Lee <dr11291129@gmail.com>
2023-11-21 14:58:17 +0900

    feat: Header 컴포넌트 구조 개선 #64
    - 각 페이지마다 사용되는 component를 구분하였습니다.
    - 이를 type을 지정하여 쉽게 사용할 수 있도록 변경하였습니다.

--- frontend/src/components/Header/components/HeaderLogo.tsx
+++ frontend/src/components/Header/components/HeaderLogo.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import logoSmall from "@/assets/imgs/logoSmall.png";
 
-interface LeftSectionProps {
-  lecture?: boolean;
+interface LogoButtonProps {
+  type: "normal" | "lecture";
 }
 
-const LeftSection = ({ lecture }: LeftSectionProps) => {
+const HeaderLogo = ({ type }: LogoButtonProps) => {
   const navigate = useNavigate();
 
   const moveToMainPage = () => {
@@ -13,14 +13,14 @@ const LeftSection = ({ lecture }: LeftSectionProps) => {
   };
 
   return (
-    <div className="flex items-center gap-4 semibold-20">
+    <>
       <button type="button" className="flex flex-row items-center gap-4" onClick={moveToMainPage}>
         <img src={logoSmall} />
-        {!lecture && <h1>Boarlog</h1>}
+        {type === "normal" && <h1>Boarlog</h1>}
       </button>
-      {lecture && <h1>lecture name</h1>}
-    </div>
+      {type === "lecture" && <h1>lecture name</h1>}
+    </>
   );
 };
 
-export default LeftSection;
+export default HeaderLogo;
---
+++ frontend/src/components/Header/components/HeaderMainButtons.tsx
@@ -0,0 +1,14 @@
+const HeaderMainButtons = () => {
+  return (
+    <>
+      <button type="button" className="medium-16">
+        시작하기
+      </button>
+      <button type="button" className="medium-16">
+        이전 강의 다시보기
+      </button>
+    </>
+  );
+};
+
+export default HeaderMainButtons;
