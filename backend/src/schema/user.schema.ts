e0d4128af2532c695c85c830453d98e4550d4a1c
tmddus2 <tmddus2__@naver.com>
2023-11-21 10:55:57 +0900

    feat: user schema 추가

---
+++ backend/src/schema/user.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  username: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  profile: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
