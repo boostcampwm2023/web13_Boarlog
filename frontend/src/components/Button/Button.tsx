2ea9b05890c4513ab9c23f075d775ec50431e11a
DongRyeong Lee <dr11291129@gmail.com>
2023-11-21 19:34:37 +0900

    feat: dialog 기본 UI 작업 #122
    - dialog 내부의 모달 UI 작업을 완료했습니다.

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -3,16 +3,18 @@ interface ButtonProps {
   onClick?: () => void;
   children: React.ReactNode;
   className?: string;
+  value?: string;
 }
 
-const Button = ({ type, onClick, className, children }: ButtonProps) => {
+const Button = ({ type, onClick, className, children, value }: ButtonProps) => {
   return (
     <button
       type="button"
-      className={`flex flex-row justify-center items-center gap-1 py-3 rounded-xl semibold-18 duration-500 ${
+      className={`flex flex-row justify-center items-center gap-1 py-3 rounded-xl semibold-18 hover:opacity-70 duration-500 ${
         type === "full" && "w-full"
-      } ${type === "fit" && "px-3 w-fit"} ${type === "grow" && "flex-grow"} ${className}`}
+      } ${type === "fit" && "px-3 w-fit"} ${type === "grow" && "flex-grow basis-0"} ${className}`}
       onClick={onClick}
+      value={value}
     >
       {children}
     </button>
