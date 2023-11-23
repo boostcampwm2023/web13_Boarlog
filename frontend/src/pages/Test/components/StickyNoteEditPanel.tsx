5d478a1fa209e41ee65a8547176e7919e953ecdb
Byeonjin <quswlstkd88@naver.com>
2023-11-23 11:10:08 +0900

    feat: 색 선택 체크 기능 구현 #49
    - 메모지 색 선택시 체크

--- frontend/src/pages/Test/components/StickyNoteEditPanel.tsx
+++ frontend/src/pages/Test/components/StickyNoteEditPanel.tsx
@@ -8,10 +8,11 @@ import AlignRightSVG from "@/assets/svgs/formatAlignRight.svg?react";
 import PalletteSVG from "@/assets/svgs/palette.svg?react";
 import DeleteMemoSVG from "@/assets/svgs/deleteMemo.svg?react";
 import CheckSVG from "@/assets/svgs/check.svg?react";
-import { MouseEventHandler, useState } from "react";
+import { useState } from "react";
 
 type FontSize = "s" | "m" | "l" | "xl";
 type FormatAlign = "left" | "center" | "right";
+type StickyNoteColor = "red" | "yellow" | "forsythia" | "lightgreen" | "blue";
 
 const ACTIVE_COLOR = "#7272fc";
 
@@ -20,21 +21,21 @@ const StickyNoteEditPanel = () => {
   const [fontSize, setFontSize] = useState<FontSize>("s");
   const [formatAlign, setFormatAlign] = useState<FormatAlign>("left");
 
-  const handlePalletteButton = () => {
+  const handlePalletteButtonClick = () => {
     setIsPalletteActive(!isPalletteActive);
   };
 
-  const handleFontSizeButton = (fontsize: FontSize) => {
+  const handleFontSizeButtonClick = (fontsize: FontSize) => {
     setFontSize(fontsize);
   };
 
-  const handleFormatAlignButton = (align: FormatAlign) => {
+  const handleFormatAlignButtonClick = (align: FormatAlign) => {
     setFormatAlign(align);
   };
 
   return (
-    <div className="">
-      <div className="w-[22.25rem] h-[2.875rem] px-4 bg-grayscale-lightgray rounded-xl flex items-center relative">
+    <div className="absolute top-2.5 left-1/2 translate-x-[-50%] ">
+      <div className="w-[22.25rem] h-[2.875rem] px-4 bg-grayscale-lightgray rounded-xl flex items-center relative shadow-md">
         {isPalletteActive && <StickyNoteColorPanel />}
         <div className="flex items-center gap-3 after:content-[''] after:block after:w-px after:h-[20px] after:bg-grayscale-gray">
           <button
@@ -43,7 +44,7 @@ const StickyNoteEditPanel = () => {
             aria-label="텍스트 크기 S"
             aria-pressed={fontSize === "s" ? true : false}
             onClick={() => {
-              handleFontSizeButton("s");
+              handleFontSizeButtonClick("s");
             }}
           >
             <SSVG style={fontSize === "s" ? { fill: ACTIVE_COLOR } : undefined} />
@@ -54,7 +55,7 @@ const StickyNoteEditPanel = () => {
             aria-label="텍스트 크기 M"
             aria-pressed={fontSize === "m" ? true : false}
             onClick={() => {
-              handleFontSizeButton("m");
+              handleFontSizeButtonClick("m");
             }}
           >
             <MSVG style={fontSize === "m" ? { fill: ACTIVE_COLOR } : undefined} />
@@ -65,7 +66,7 @@ const StickyNoteEditPanel = () => {
             aria-label="텍스트 크기 L"
             aria-pressed={fontSize === "l" ? true : false}
             onClick={() => {
-              handleFontSizeButton("l");
+              handleFontSizeButtonClick("l");
             }}
           >
             <LSVG style={fontSize === "l" ? { fill: ACTIVE_COLOR } : undefined} />
@@ -76,7 +77,7 @@ const StickyNoteEditPanel = () => {
             aria-label="텍스트 크기 XL"
             aria-pressed={fontSize === "xl" ? true : false}
             onClick={() => {
-              handleFontSizeButton("xl");
+              handleFontSizeButtonClick("xl");
             }}
           >
             <XLSVG style={fontSize === "xl" ? { fill: ACTIVE_COLOR } : undefined} />
@@ -89,7 +90,7 @@ const StickyNoteEditPanel = () => {
             aria-label="텍스트 좌측 정렬"
             aria-pressed={formatAlign === "left" ? true : false}
             onClick={() => {
-              handleFormatAlignButton("left");
+              handleFormatAlignButtonClick("left");
             }}
           >
             <AlignLeftSVG style={formatAlign === "left" ? { fill: ACTIVE_COLOR } : undefined} />
@@ -100,7 +101,7 @@ const StickyNoteEditPanel = () => {
             aria-label="텍스트 중앙 정렬"
             aria-pressed={formatAlign === "center" ? true : false}
             onClick={() => {
-              handleFormatAlignButton("center");
+              handleFormatAlignButtonClick("center");
             }}
           >
             <AlignCenterSVG style={formatAlign === "center" ? { fill: ACTIVE_COLOR } : undefined} />
@@ -111,7 +112,7 @@ const StickyNoteEditPanel = () => {
             aria-label="텍스트 우측 정렬"
             aria-pressed={formatAlign === "right" ? true : false}
             onClick={() => {
-              handleFormatAlignButton("right");
+              handleFormatAlignButtonClick("right");
             }}
           >
             <AlignRightSVG style={formatAlign === "right" ? { fill: ACTIVE_COLOR } : undefined} />
@@ -123,12 +124,12 @@ const StickyNoteEditPanel = () => {
             className="h-6 w-6 bg-transparent flex justify-center items-center"
             aria-label="메모지 컬러 팔레트 켜고 닫기"
             aria-pressed={isPalletteActive}
-            aria-aria-expanded={isPalletteActive}
+            aria-expanded={isPalletteActive}
             onClick={() => {
-              handlePalletteButton();
+              handlePalletteButtonClick();
             }}
           >
-            <PalletteSVG />
+            <PalletteSVG style={isPalletteActive ? { fill: ACTIVE_COLOR } : undefined} />
           </button>
           <button
             type="button"
@@ -144,42 +145,63 @@ const StickyNoteEditPanel = () => {
 };
 
 const StickyNoteColorPanel = () => {
+  const [stickyNoteColor, setStickyNoteColor] = useState<StickyNoteColor>("yellow");
+
+  const handleColorButtonClick = (color: StickyNoteColor) => {
+    setStickyNoteColor(color);
+  };
+
   return (
-    <div className="w-[9.5rem] h-[2.5rem] p-2 rounded-xl flex items-center gap-1 bg-grayscale-lightgray right-[2.75rem] top-[-3.125rem] absolute ">
+    <div className="w-[9.5rem] h-[2.5rem] p-2 rounded-xl flex items-center gap-1 bg-grayscale-lightgray right-[2.75rem] bottom-[-3.125rem] absolute shadow-md">
       <button
         type="button"
         className="rounded-[10px] h-6 w-6 border flex justify-center items-center border-memo-border-red bg-memo-red"
         aria-label="빨간색"
+        onClick={() => {
+          handleColorButtonClick("red");
+        }}
       >
-        <CheckSVG />
+        {stickyNoteColor === "red" && <CheckSVG />}
       </button>
       <button
         type="button"
         className="rounded-[10px] h-6 w-6 border flex justify-center items-center border-memo-border-yellow bg-memo-yellow"
         aria-label="노란색"
+        onClick={() => {
+          handleColorButtonClick("yellow");
+        }}
       >
-        <CheckSVG />
+        {stickyNoteColor === "yellow" && <CheckSVG />}
       </button>
       <button
         type="button"
         className="rounded-[10px] h-6 w-6 border flex justify-center items-center border-memo-border-forsythia bg-memo-forsythia"
         aria-label="개나리색"
+        onClick={() => {
+          handleColorButtonClick("forsythia");
+        }}
       >
-        <CheckSVG />
+        {stickyNoteColor === "forsythia" && <CheckSVG />}
       </button>
       <button
         type="button"
         className="rounded-[10px] h-6 w-6 border flex justify-center items-center border-memo-border-lightgreen bg-memo-lightgreen"
         aria-label="연녹색"
+        onClick={() => {
+          handleColorButtonClick("lightgreen");
+        }}
       >
-        <CheckSVG />
+        {stickyNoteColor === "lightgreen" && <CheckSVG />}
       </button>
       <button
         type="button"
         className="rounded-[10px] h-6 w-6 border flex justify-center items-center border-memo-border-blue bg-memo-blue"
         aria-label="파란색"
+        onClick={() => {
+          handleColorButtonClick("blue");
+        }}
       >
-        <CheckSVG />
+        {stickyNoteColor === "blue" && <CheckSVG />}
       </button>
     </div>
   );
