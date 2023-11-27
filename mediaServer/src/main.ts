1b8ba5e68eb9fd49c423877b0b74fd9f94002c18
tmddus2 <tmddus2__@naver.com>
2023-11-27 17:57:23 +0900

    test: test용 html 파일 응답하도록 구현

--- mediaServer/src/main.ts
+++ mediaServer/src/main.ts
@@ -1,7 +1,22 @@
 import { RelayServer } from './RelayServer';
+import * as http from 'http';
+import fs from 'fs';
 
 const PORT = 3000;
 
 const relayServer = new RelayServer(PORT);
 relayServer.listen('/create-room', 'connection', relayServer.createRoom);
 relayServer.listen('/enter-room', 'connection', relayServer.enterRoom);
+
+const app = http.createServer((req, res) => {
+  try {
+    fs.readFile(__dirname + '/../' + req.url, (err, file) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(file);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.listen(8080);
