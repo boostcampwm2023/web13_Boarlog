ffb88f09c85f8847a6c18a3ee8b38f2e7b433c74
DongRyeong Lee <dr11291129@gmail.com>
2023-11-28 14:27:55 +0900

    feat: 강의 코드 복사 기능 구현 #68
    - 버튼 클릭 시에 강의 코드를 복사하도록 작업을 완료했습니다.

--- frontend/src/components/Header/components/HeaderCodeCopyButton.tsx
+++ frontend/src/components/Header/components/HeaderCodeCopyButton.tsx
@@ -1,13 +1,27 @@
 import SmallButton from "@/components/SmallButton/SmallButton";
 import ShareIcon from "@/assets/svgs/share.svg?react";
+import { useToast } from "@/components/Toast/useToast";
 
 interface HeaderCodeCopyButtonProps {
   lectureCode: string;
 }
 
 const HeaderCodeCopyButton = ({ lectureCode }: HeaderCodeCopyButtonProps) => {
+  const showToast = useToast();
+  const handleShareButtonClicked = async () => {
+    try {
+      await navigator.clipboard.writeText(
+        `[함께 듣는 실시간 강의 Boarlog]\n\n지금 진행되는 강의에 참여해보세요.\n\n강의 코드: ${lectureCode}\n강의 제목: 강의 제목\n강의 링크: 강의 링크`
+      );
+      showToast({ message: "클립보드에 강의 코드를 복사했어요.", type: "success" });
+    } catch (error) {
+      console.error("복사 실패:", error);
+      showToast({ message: "강의 코드 복사에 실패했어요. 잠시 후 다시 시도해 주세요.", type: "alert" });
+    }
+  };
+
   return (
-    <SmallButton onClick={() => console.log("clicked")} className="bg-grayscale-lightgray">
+    <SmallButton onClick={handleShareButtonClicked} className="bg-grayscale-lightgray">
       <ShareIcon className="fill-grayscale-black" />
       {lectureCode}
     </SmallButton>
