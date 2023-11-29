b0d73f9498e4b83bae0d9c901b9f519bd586a2bc
DongRyeong Lee <dr11291129@gmail.com>
2023-11-29 18:13:15 +0900

    feat: 마이 페이지 기본 UI 작업 #76
    - 마이 페이지의 기본 UI를 구성하였습니다.

---
+++ frontend/src/pages/MyPage/components/MyPageSection.tsx
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import Button from "@/components/Button/Button";
+import ProfileBig from "@/assets/imgs/profileBig.png";
+import EnterIcon from "@/assets/svgs/enter.svg?react";
+import { useToast } from "@/components/Toast/useToast";
+import ReplayLectureCard from "./ReplayLectureCard";
+
+interface MyPageSectionProps {
+  profileImage: string;
+}
+
+const NICKNAME_REGEXP = /^(?![0-9-_.]+$)[가-힣A-Za-z0-9-_.]{1,10}$/;
+
+const MyPageSection = ({ profileImage }: MyPageSectionProps) => {
+  const showToast = useToast();
+  const [nickname, setNickname] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    showToast({ message: "닉네임을 설정해주세요.", type: "default" });
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newNickname = event.target.value;
+    setNickname(newNickname);
+    setIsValid(NICKNAME_REGEXP.test(newNickname));
+  };
+
+  const handleStartButtonClicked = () => {
+    if (isValid) {
+      showToast({ message: "닉네임 변경을 완료했습니다.", type: "success" });
+    } else {
+      showToast({ message: "올바르지 않은 닉네임입니다.", type: "alert" });
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center mt-32 st:mt-36">
+      <section className="flex relative w-11/12 max-w-3xl p-8 flex-col items-center gap-8 rounded-2xl border-default shadow-xl">
+        <img
+          src={profileImage ? profileImage : ProfileBig}
+          alt="프로필 이미지"
+          className="absolute -top-20 w-40 h-40 st:w-56 st:h-56 st:-top-28"
+        />
+
+        <input
+          type="text"
+          value={nickname}
+          onChange={handleChange}
+          size={nickname.length || 1}
+          placeholder="닉네임"
+          maxLength={10}
+          className="mt-20 st:mt-28 semibold-32 text-center border-b-2 border-grayscale-gray focus:border-grayscale-black outline-none transition duration-200"
+        />
+
+        <div className="flex flex-col gap-1 justify-center items-center">
+          <p className="semibold-18 text-grayscale-darkgray">사용할 닉네임을 입력해 주세요.</p>
+          <p className={`medium-12 ${isValid ? "text-boarlog-100" : "text-alert-100"}`}>
+            한글, 영문, 숫자, -, _, ., 총 10자 이내
+          </p>
+        </div>
+
+        <div className="w-full max-w-sm">
+          <Button type="full" buttonStyle={isValid ? "blue" : "black"} onClick={handleStartButtonClicked}>
+            <EnterIcon className="fill-grayscale-white" />
+            볼록 시작하기
+          </Button>
+        </div>
+
+        <h3 className="mt-12 semibold-32">강의 다시보기</h3>
+        <div className="flex flex-col w-full gap-6">
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+          <ReplayLectureCard />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MyPageSection;
---
+++ frontend/src/pages/MyPage/components/ReplayLectureCard.tsx
@@ -0,0 +1,36 @@
+import ProfileMedium from "@/assets/imgs/profileMedium.png";
+import UserIcon from "@/assets/svgs/user.svg?react";
+import CalendarIcon from "@/assets/svgs/calendar.svg?react";
+import PlayIcon from "@/assets/svgs/play.svg?react";
+
+const ReplayLectureCard = () => {
+  return (
+    <div className="flex flex-col w-full p-6 gap-4 justify-between bg-grayscale-white border-default rounded-xl hover:shadow-xl duration-500 cursor-pointer break-keep">
+      <div className="flex flex-row gap-3 w-full items-center">
+        <img src={ProfileMedium} alt="강의 진행자 프로필" className="w-12 h-12" />
+        <div className="flex flex-col w-full gap-1 justify-center items-left semibold-20">
+          React Hooks 알아보기
+          <div className="flex flex-row gap-1 items-center medium-16 text-grayscale-darkgray">
+            <UserIcon className="w-4 h-4 fill-grayscale-darkgray" />
+            볼록이
+          </div>
+        </div>
+      </div>
+      <p className="medium-16 text-grayscale-darkgray">
+        앞으로 그룹 프로젝트에서 활용할 React Hooks에 대해 간단히 알아봅시다. useEffect와 useState에 대해서 학습합니다.
+      </p>
+      <div className="flex flex-row justify-between w-full items-center">
+        <div className="flex flex-row gap-1 semibold-16 text-boarlog-100">
+          <CalendarIcon className="w-4 h-4 fill-boarlog-100" />
+          2023.00.00 00:00
+        </div>{" "}
+        <div className="flex flex-row gap-1 semibold-16 text-alert-100">
+          <PlayIcon className="w-4 h-4 fill-alert-100" />
+          00:00
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReplayLectureCard;
