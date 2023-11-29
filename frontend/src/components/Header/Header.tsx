af5d619406351e0908f9b4d91a81c78809d2f94f
DongRyeong Lee <dr11291129@gmail.com>
2023-11-29 17:20:38 +0900

    chore: header 높이 고정 #76
    - header의 높이를 5rem으로 고정하였습니다.

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -17,7 +17,7 @@ const Header = ({ type }: HeaderProps) => {
   const [isSettingClicked, setIsSettingClicked] = useState(false);
 
   return (
-    <header className="flex w-100 relative items-center justify-between px-6 py-4 bg-grayscale-white border-header box-border z-10">
+    <header className="flex w-100 relative h-20 items-center justify-between px-6 py-4 bg-grayscale-white border-header box-border z-10">
       <div className="flex items-center gap-4 semibold-20">
         {(type === "login" || type === "main") && <HeaderLogo type="normal" />}
         {(type === "instructor" || type === "participant") && (
