d5bbf825d018c22102d43396805da7c33a563dce
DongRyeong Lee <dr11291129@gmail.com>
2023-11-30 12:03:08 +0900

    feat: tailwind.config 수정 #76
    - 반응형 수정으로 불필요한 속성을 제거하였습니다.
    - home에 적용되는 반응형 태그를 명시적으로 변경하였습니다.

--- frontend/src/pages/Home/components/JoinLectureFront.tsx
+++ frontend/src/pages/Home/components/JoinLectureFront.tsx
@@ -16,7 +16,7 @@ const JoinLectureFront = ({ handleJoinLectureTrue }: JoinLectureFrontProps) => {
       <div className="w-full flex grow flex-col p-6 justify-between">
         <div>
           <h3 className="semibold-32 break-keep mb-1">강의 참여하기</h3>
-          <p className="medium-18 text-grayscale-darkgray break-keep">
+          <p className="medium-16 text-grayscale-darkgray break-keep">
             생성된 강의 세션에 참여해 보세요. 실시간 채팅으로 세션 진행자에게 질문하고 다양한 사람들과 소통할 수 있어요.
           </p>
         </div>
