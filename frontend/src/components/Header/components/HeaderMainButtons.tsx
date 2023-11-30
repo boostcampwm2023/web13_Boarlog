f7bc744db61de5bc820560b35032a2784140d9bf
DongRyeong Lee <dr11291129@gmail.com>
2023-11-30 15:18:09 +0900

    feat: 정적 페이지 연결 #76
    - 메인 페이지에서 다른 페이지로 쉽게 넘어갈 수 있도록 페이지를 정적으로
      연결하였습니다.

--- frontend/src/components/Header/components/HeaderMainButtons.tsx
+++ frontend/src/components/Header/components/HeaderMainButtons.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 const HeaderMainButtons = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <button type="button" className="medium-16">
+      <button type="button" className="medium-16" onClick={() => navigate("/")}>
         시작하기
       </button>
-      <button type="button" className="medium-16">
+      <button type="button" className="medium-16" onClick={() => navigate("/mypage")}>
         강의 다시보기
       </button>
     </>
