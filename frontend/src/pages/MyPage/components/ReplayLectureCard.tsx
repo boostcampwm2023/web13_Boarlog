8c27997a9697e4d4c3ef9d36a99d41d8eb9057af
DongRyeong Lee <dr11291129@gmail.com>
2023-11-30 13:55:18 +0900

    feat: mypage 더미 데이터 추가 #76
    - 마이 페이지에 더미 데이터를 추가하였습니다.
    - 더미 데이터를 기준으로 강의 리스트가 표시되도록 하였습니다.

--- frontend/src/pages/MyPage/components/ReplayLectureCard.tsx
+++ frontend/src/pages/MyPage/components/ReplayLectureCard.tsx
@@ -3,30 +3,39 @@ import UserIcon from "@/assets/svgs/user.svg?react";
 import CalendarIcon from "@/assets/svgs/calendar.svg?react";
 import PlayIcon from "@/assets/svgs/play.svg?react";
 
-const ReplayLectureCard = () => {
+interface ReplayLectureCardProps {
+  profileImage: string;
+  date: string;
+  duration: string;
+  user: string;
+  title: string;
+  description: string;
+}
+
+const ReplayLectureCard = ({ profileImage, date, duration, user, title, description }: ReplayLectureCardProps) => {
   return (
     <div className="flex flex-col w-full p-6 gap-4 justify-between bg-grayscale-white border-default rounded-xl hover:shadow-xl duration-500 cursor-pointer break-keep">
       <div className="flex flex-row gap-3 w-full items-center">
-        <img src={ProfileMedium} alt="강의 진행자 프로필" className="w-12 h-12" />
+        <img src={profileImage ? profileImage : ProfileMedium} alt="강의 진행자 프로필" className="w-12 h-12" />
         <div className="flex flex-col w-full gap-1 justify-center items-left semibold-20">
-          React Hooks 알아보기
+          {title}
           <div className="flex flex-row gap-1 items-center medium-16 text-grayscale-darkgray">
             <UserIcon className="w-4 h-4 fill-grayscale-darkgray" />
-            볼록이
+            {user}
           </div>
         </div>
       </div>
-      <p className="medium-16 text-grayscale-darkgray">
-        앞으로 그룹 프로젝트에서 활용할 React Hooks에 대해 간단히 알아봅시다. useEffect와 useState에 대해서 학습합니다.
-      </p>
+
+      <p className="medium-16 text-grayscale-darkgray">{description}</p>
+
       <div className="flex flex-row justify-between w-full items-center">
         <div className="flex flex-row gap-1 items-center semibold-16 text-boarlog-100">
           <CalendarIcon className="w-4 h-4 fill-boarlog-100" />
-          2023.00.00 00:00
+          {date}
         </div>{" "}
         <div className="flex flex-row gap-1 items-center semibold-16 text-alert-100">
           <PlayIcon className="w-4 h-4 fill-alert-100" />
-          00:00
+          {duration}
         </div>
       </div>
     </div>
