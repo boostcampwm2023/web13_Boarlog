a1ac464dbe3d50e7637468bce1d23b928cbee298
DongRyeong Lee <dr11291129@gmail.com>
2023-11-30 13:55:18 +0900

    feat: mypage 더미 데이터 추가 #76
    - 마이 페이지에 더미 데이터를 추가하였습니다.
    - 더미 데이터를 기준으로 강의 리스트가 표시되도록 하였습니다.

--- frontend/src/pages/MyPage/components/MyPageSection.tsx
+++ frontend/src/pages/MyPage/components/MyPageSection.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Button from "@/components/Button/Button";
 import ProfileBig from "@/assets/imgs/profileBig.png";
 import EnterIcon from "@/assets/svgs/enter.svg?react";
@@ -10,6 +10,89 @@ interface MyPageSectionProps {
 }
 
 const NICKNAME_REGEXP = /^(?![0-9-_.]+$)[가-힣A-Za-z0-9-_.]{1,10}$/;
+
+const DUMMY_DATA = [
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.11",
+    title: "프로그래밍 기초",
+    description: "컴퓨터 프로그래밍의 기본 원리를 배우는 입문 강좌. 언어 선택부터 기본 구문까지."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.12",
+    title: "요리의 기초",
+    description: "기본적인 요리 기술과 요리법을 배우는 강좌. 초보자를 위한 쉬운 레시피와 요리 팁 제공."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.13",
+    title: "디지털 마케팅",
+    description: "디지털 마케팅 전략과 소셜 미디어 활용 방법을 배우는 실용적인 강좌."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.14",
+    title: "수학의 이해",
+    description: "기본적인 수학 개념과 문제 해결 기술을 배우는 강좌. 수학에 대한 두려움을 극복할 수 있도록 도움."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.15",
+    title: "역사 탐험",
+    description: "세계 역사의 주요 사건과 인물을 탐구하는 강좌. 역사를 통해 현재를 이해하는 데 도움."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.16",
+    title: "창의적 글쓰기",
+    description: "창의적인 글쓰기 기술과 아이디어 발상법을 배우는 강좌. 글쓰기를 통한 자기 표현 방법 탐구."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.17",
+    title: "영화 분석",
+    description: "영화의 기술적 요소와 예술적 가치를 분석하는 강좌. 영화를 깊이 있게 이해하는 방법 제공."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.18",
+    title: "피트니스 가이드",
+    description: "건강하고 효율적인 운동 방법을 배우는 강좌. 개인별 맞춤 운동 계획 설정 방법 제공."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.19",
+    title: "사진술 입문",
+    description: "기초 사진 기술과 구도, 조명 활용법을 배우는 강좌. 사진을 통해 예술적 감각 향상."
+  },
+  {
+    profileImage: "",
+    duration: "12:34",
+    user: "볼록이",
+    date: "2023.11.20",
+    title: "환경 보호",
+    description: "환경 보호와 지속 가능한 생활 방식에 대해 배우는 강좌. 일상에서 실천할 수 있는 환경 보호 활동 소개."
+  }
+];
 
 const MyPageSection = ({ profileImage }: MyPageSectionProps) => {
   const showToast = useToast();
@@ -65,17 +148,17 @@ const MyPageSection = ({ profileImage }: MyPageSectionProps) => {
 
         <h3 className="mt-12 semibold-32">강의 다시보기</h3>
         <div className="flex flex-col w-full gap-6">
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
-          <ReplayLectureCard />
+          {DUMMY_DATA.map((value, index) => (
+            <ReplayLectureCard
+              key={index}
+              profileImage={value.profileImage}
+              date={value.date}
+              duration={value.duration}
+              user={value.user}
+              title={value.title}
+              description={value.description}
+            />
+          ))}
         </div>
       </section>
     </div>
