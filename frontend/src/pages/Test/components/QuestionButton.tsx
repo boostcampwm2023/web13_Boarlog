b5050fe3486c6baa34ef61e96638aa1c387b12d2
Byeonjin <quswlstkd88@naver.com>
2023-11-30 14:19:34 +0900

    feat: 새로운 질문의 수를 세는 IndicatorBubble UI 구현 #55
    - 새로운 질문의 수를 세는 버블 UI구현
    - 0개라면 버블이 표시되지 않도록 구현

--- frontend/src/pages/Test/components/QuestionButton.tsx
+++ frontend/src/pages/Test/components/QuestionButton.tsx
@@ -3,13 +3,24 @@ import { useRecoilState, useSetRecoilState } from "recoil";
 import isQuestionListOpenState from "./stateIsQuestionListOpen";
 import activeToolState from "./stateActiveTool";
 
+const IndicatorBubble = ({ count }: { count: number }) => {
+  return (
+    <div className="absolute -top-2 -right-2 flex justify-center items-center rounded-xl w-5 h-5  bg-alert-100 medium-12 text-grayscale-white">
+      {count}
+    </div>
+  );
+};
+
 const QuestionButton = () => {
   const [isQuestionListOpen, setIsQuestionListOpen] = useRecoilState(isQuestionListOpenState);
   const setActiveTool = useSetRecoilState(activeToolState);
+  // 후에 실시간으로 들어오는 질문의 수를 카운팅 하도록 구현 해야함.
+  let newQuestionCount = 10;
+
   return (
     <button
       type="button"
-      className={`w-12 h-12  shadow-md flex justify-center items-center rounded-xl mb-3 ${
+      className={`relative w-12 h-12  shadow-md flex justify-center items-center rounded-xl mb-3 ${
         isQuestionListOpen ? "bg-boarlog-80" : "bg-grayscale-lightgray"
       }`}
       aria-label="질문 리스트"
@@ -19,6 +30,8 @@ const QuestionButton = () => {
         setIsQuestionListOpen(!isQuestionListOpen);
       }}
     >
+      {newQuestionCount !== 0 && <IndicatorBubble count={newQuestionCount} />}
+
       <QuestionIcon className={isQuestionListOpen ? "fill-white" : "fill-grayscale-black"} />
     </button>
   );
