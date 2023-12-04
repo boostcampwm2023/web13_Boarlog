cfbed70a30d439a7df6544ae5570be5d70aa562b
DongRyeong Lee <dr11291129@gmail.com>
2023-12-04 19:35:36 +0900

    feat: home으로 navigate 추가 #24
    - 홈 화면으로 돌아가기 버튼 클릭 시에 Home으로 이동하도록 navigate를
      추가하였습니다.

--- frontend/src/pages/Error/Error.tsx
+++ frontend/src/pages/Error/Error.tsx
@@ -1,8 +1,11 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import LogoBig from "@/assets/imgs/logoBig.png";
 import Button from "@/components/Button/Button";
 import HomeIcon from "@/assets/svgs/home.svg?react";
 
 const Error = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center justify-center display-center w-11/12 sm:8/12 md:w-fit">
       <div className="flex flex-row items-center justify-center">
@@ -12,7 +15,7 @@ const Error = () => {
       </div>
       <p className="font-pretendard font-semibold text-size-32 md:text-size-64">NOT FOUND</p>
       <p className="semibold-20 text-grayscale-darkgray mt-12 mb-6">이런! 잘못된 접근이에요.</p>
-      <Button type="full" buttonStyle="black">
+      <Button type="full" buttonStyle="black" onClick={() => navigate("/")}>
         <HomeIcon className="fill-grayscale-white" />홈 화면으로 돌아가기
       </Button>
     </div>
