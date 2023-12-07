f5b21464b3d806fd0fdc4b27660e27f955461a0e
Juwan <sirius_75@naver.com>
2023-12-07 12:33:01 +0900

    fix: modal을 클릭 할 수 있게 z-index 수정
    modal의 z-index가 낮아서 참여자 페이지에서 모달을 클릭할 수 없던 문제를
    개선했습니다.

--- frontend/src/pages/Participant/Participant.tsx
+++ frontend/src/pages/Participant/Participant.tsx
@@ -48,14 +48,14 @@ const Participant = () => {
   return (
     <>
       <Header type="participant" />
-      <section className="relative w-screen h-[calc(100vh-5rem)]" ref={canvasContainerRef}>
+      <section className="relative -z-20 w-screen h-[calc(100vh-5rem)]" ref={canvasContainerRef}>
         <canvas ref={canvasRef} />
-        <div className="absolute z-10 w-screen h-[calc(100vh-5rem)] top-0 bg-transparent"></div>
+        <div className="absolute w-screen h-[calc(100vh-5rem)] top-0 bg-transparent"></div>
         <LogContainer
           type="question"
-          className={`absolute top-2.5 right-2.5 z-20 ${isQuestionLogOpen ? "block" : "hidden"}`}
+          className={`absolute top-2.5 right-2.5 z-10 ${isQuestionLogOpen ? "block" : "hidden"}`}
         />
-        <QuestionLogButton className="absolute top-2.5 right-2.5 z-20" />
+        <QuestionLogButton className="absolute top-2.5 right-2.5 z-10" />
       </section>
     </>
   );
