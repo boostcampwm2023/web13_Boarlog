835e0e1b4ca3371641d916c579598bf0b5d831bb
Byeonjin <quswlstkd88@naver.com>
2023-12-07 11:53:37 +0900

    refact: convertMsToTimeString 함수 내 매직넘버 상수화

--- frontend/src/utils/convertMsToTimeString.ts
+++ frontend/src/utils/convertMsToTimeString.ts
@@ -1,10 +1,14 @@
+const MS_OF_SECOND = 1000;
+const SECOND_OF_HOUR = 3600;
+const MINUTE_OF_HOUR = 60;
+
 const convertMsToTimeString = (ms: string) => {
   let msNumber = parseInt(ms);
-  let seconds = Math.floor(msNumber / 1000);
-  let hours = Math.floor(seconds / 3600);
+  let seconds = Math.floor(msNumber / MS_OF_SECOND);
+  let hours = Math.floor(seconds / SECOND_OF_HOUR);
   seconds = seconds % 3600;
 
-  let minutes = Math.floor(seconds / 60);
+  let minutes = Math.floor(seconds / MINUTE_OF_HOUR);
   seconds = seconds % 60;
 
   return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
