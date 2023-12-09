d82e53bed35e853023f513a8c5a52f6af1f2d1fd
DongRyeong Lee <dr11291129@gmail.com>
2023-12-09 15:00:31 +0900

    feat: login 페이지 이름 변경, App.tsx 코드 정리 #236
    - login 페이지 이름을 UserAuth로 변경하였습니다.
    - App.tsx 코드를 순서에 맞게 정리하였습니다.

---
+++ frontend/src/pages/UserAuth/UserAuth.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header/Header";
+import UserAuthSection from "./components/UserAuthSection";
+
+const UserAuth = () => {
+  return (
+    <>
+      <Header type="login" />
+      <UserAuthSection />
+    </>
+  );
+};
+
+export default UserAuth;
