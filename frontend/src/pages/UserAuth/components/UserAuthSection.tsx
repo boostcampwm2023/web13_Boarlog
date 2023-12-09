db711ba692ed4427f74720f281c185fa88cc8076
DongRyeong Lee <dr11291129@gmail.com>
2023-12-09 16:49:56 +0900

    feat: 로그인 UI 처리 #236
    - 로그인 UI 작업을 완료하였습니다.

--- frontend/src/pages/UserAuth/components/UserAuthSection.tsx
+++ frontend/src/pages/UserAuth/components/UserAuthSection.tsx
@@ -1,26 +1,54 @@
 import { useNavigate } from "react-router-dom";
-import logoBig from "@/assets/imgs/logoBig.png";
-import GoogleIcon from "@/assets/svgs/google.svg?react";
+import EnterIcon from "@/assets/svgs/enter.svg?react";
+import UserIcon from "@/assets/svgs/user.svg?react";
 import Button from "@/components/Button/Button";
+import LogoOriginal from "@/assets/imgs/logoOriginal.png";
+import SubLogoOriginal from "@/assets/imgs/subLogoOriginal.png";
 
 const UserAuthSection = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-1 items-center justify-center h-[calc(100vh-5rem)]">
-      <section className="flex relative w-11/12 max-w-xl p-6 flex-col items-center gap-4 rounded-xl border-default shadow-xl">
-        <img src={logoBig} alt="Boarlog 로고" className="absolute -top-20 w-40 h-40 sm:w-56 sm:h-56 sm:-top-28"></img>
-        <h2 className="semibold-32 mt-20 sm:mt-28">Boarlog</h2>
+    <div className="flex flex-col relative py-20 sm:py-32 items-center justify-center w-full overflow-x-hidden overflow-y-visible">
+      <section className="flex relative w-11/12 sm:max-w-md p-8 bg-grayscale-white flex-col items-start gap-8 rounded-xl border-default shadow-xl">
+        <img src={LogoOriginal} className="absolute hidden -top-2 -left-52 rotate-[-20deg] z-[-1] w-64 sm:block" />
+        <img src={SubLogoOriginal} className="absolute hidden -bottom-16 -right-80 rotate-[20deg] z-[-1] sm:block" />
 
-        <div className="flex flex-col gap-1 justify-center items-center">
-          <h3 className="semibold-18 text-grayscale-darkgray">기록으로 남기는 실시간 강의</h3>
-          <p className="medium-12 text-grayscale-darkgray">지금 바로 볼록과 함께하세요.</p>
+        <div className="flex flex-col">
+          <h1 className="semibold-64">Boarlog</h1>
+          <h2 className="semibold-20 text-grayscale-darkgray">기록으로 남기는 실시간 강의</h2>
         </div>
 
-        <div className="w-full max-w-sm">
-          <Button type="full" buttonStyle="black" onClick={() => navigate("/start")}>
-            <GoogleIcon />
-            구글 계정으로 로그인
+        <div className="flex flex-col gap-4 w-full">
+          <h3 className="semibold-32">로그인</h3>
+          <div className="flex flex-col gap-2 w-full">
+            <p className="semibold-18">이메일</p>
+            <input
+              type="text"
+              className="rounded-xl border-black w-full flex-grow medium-12 p-3 focus:outline-none focus:ring-1 focus:ring-boarlog-100 focus:border-transparent"
+              placeholder="이메일을 입력해주세요"
+              maxLength={50}
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full">
+            <p className="semibold-18">비밀번호</p>
+            <input
+              type="text"
+              className="rounded-xl border-black w-full flex-grow medium-12 p-3 focus:outline-none focus:ring-1 focus:ring-boarlog-100 focus:border-transparent"
+              placeholder="비밀번호을 입력해주세요"
+              maxLength={50}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col w-full gap-4 sm:flex-row">
+          <Button type="grow" buttonStyle="black" onClick={() => navigate("/start")}>
+            <EnterIcon className="fill-grayscale-white" />
+            시작하기
+          </Button>
+          <Button type="grow" buttonStyle="black" onClick={() => navigate("/start")}>
+            <UserIcon className="fill-grayscale-white" />
+            회원가입
           </Button>
         </div>
       </section>
