da70f33233550bf1630b3b1430ae1f091496cc5a
DongRyeong Lee <dr11291129@gmail.com>
2023-12-11 17:10:52 +0900

    feat: header profile 유저 정보 로직 추가 #254
    - accessToken의 유효 여부에 따라 구분하여 정보를 localStorage에
      저장하도록 하였습니다.

--- frontend/src/components/Header/components/HeaderProfileButton.tsx
+++ frontend/src/components/Header/components/HeaderProfileButton.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useToast } from "@/components/Toast/useToast";
 import HeaderProfileModal from "./HeaderProfileModal";
 import ProfileSmall from "@/assets/imgs/profileSmall.png";
 
@@ -7,6 +11,39 @@ interface HeaderProfileButtonProps {
 }
 
 const HeaderProfileButton = ({ isProfileClicked, setIsProfileClicked }: HeaderProfileButtonProps) => {
+  const navigate = useNavigate();
+  const showToast = useToast();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("token");
+    if (!accessToken) {
+      showToast({ message: "로그인이 필요해요.", type: "alert" });
+      navigate("/userauth");
+    } else {
+      const username = localStorage.getItem("username");
+      const email = localStorage.getItem("email");
+      if (!username || !email) {
+        axios
+          .get(`${import.meta.env.VITE_API_SERVER_URL}/profile`, {
+            headers: {
+              Authorization: accessToken
+            }
+          })
+          .then((result) => {
+            console.log(result);
+            localStorage.setItem("username", result.data.username);
+            localStorage.setItem("email", result.data.email);
+          })
+          .catch((error) => {
+            if (error.response.status === 401) {
+              showToast({ message: "로그인이 만료되었어요.", type: "alert" });
+              navigate("/userauth");
+            }
+          });
+      }
+    }
+  }, []);
+
   return (
     <>
       <button type="button" className="" onClick={() => setIsProfileClicked(!isProfileClicked)}>
