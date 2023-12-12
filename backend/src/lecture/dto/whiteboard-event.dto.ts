02214b67a3f7571363a4a75b6f5c0f3d5331305b
tmddus2 <tmddus2__@naver.com>
2023-12-12 19:03:49 +0900

    refactor: API requestDTO 형식 수정

--- backend/src/lecture/dto/whiteboard-event.dto.ts
+++ backend/src/lecture/dto/whiteboard-event.dto.ts
@@ -1,16 +1,11 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 export class WhiteboardEventDto {
-  @ApiProperty({
-    example: '"canvasJSON": "{\\"version\\":\\"5.3.0\\",\\"objects\\":[{\\"type\\":\\"path\\", ...'
-  })
   canvasJSON: string;
 
-  @ApiProperty({
-    example: '[0.9999999999999875, 0, 0, 0.9999999999999875, 171.2148298216324, 100.8520614096598]'
-  })
-  viewPort: number[];
+  viewport: number[];
 
-  @ApiProperty({ example: '123456' })
-  eventDate: string;
+  eventTime: number;
+
+  width: number;
+
+  height: number;
 }
--- backend/src/lecture/schema/whiteboard-log.schema.ts
+++ backend/src/lecture/schema/whiteboard-log.schema.ts
@@ -10,10 +10,16 @@ export class WhiteboardLog {
   canvasJSON: string;
 
   @Prop({ required: true })
-  viewPort: number[];
+  viewport: number[];
 
   @Prop({ required: true })
-  eventDate: string;
+  eventTime: number;
+
+  @Prop({ required: true })
+  width: number;
+
+  @Prop({ required: true })
+  height: number;
 
   @Prop({ required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Lecture' })
   lecture_id: Lecture;
