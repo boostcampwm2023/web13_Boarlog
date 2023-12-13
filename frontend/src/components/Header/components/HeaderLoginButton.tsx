a31b17f40dd07c0160379fe520c45c453d40c30b
DongRyeong Lee <dr11291129@gmail.com>
2023-12-13 17:36:54 +0900

    feat: userauth 우측 header 문구 수정, 클릭 시 이동 #288
    - Guest로 서비스를 이용할 수 있도록 문구를 수정하고 이동하도록
      처리하였습니다.

--- frontend/src/components/Header/components/HeaderLoginButton.tsx
+++ frontend/src/components/Header/components/HeaderLoginButton.tsx
@@ -1,7 +1,17 @@
+import { useToast } from "@/components/Toast/useToast";
+import { useNavigate } from "react-router-dom";
+
 const HeaderLoginButton = () => {
+  const navigate = useNavigate();
+  const showToast = useToast();
+  const handleButtonClicked = () => {
+    showToast({ message: "Guest로 시작합니다.", type: "default" });
+    navigate("/");
+  };
+
   return (
-    <button type="button" className="medium-16">
-      Boarlog 체험하기
+    <button type="button" className="medium-16" onClick={handleButtonClicked}>
+      Guest로 시작하기
     </button>
   );
 };
