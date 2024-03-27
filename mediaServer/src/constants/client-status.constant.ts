1463a944b5e6e7f42c8a2dba7c6dc540ba43c9c6
platinouss <platinouss@gmail.com>
2024-03-27 22:23:12 +0900

    feat: 클라이언트 online/offline 상태 추가

---
+++ mediaServer/src/constants/client-status.constant.ts
@@ -0,0 +1,4 @@
+export enum ClientStatus {
+  ONLINE,
+  OFFLINE
+}
--- mediaServer/src/models/ClientConnectionInfo.ts
+++ mediaServer/src/models/ClientConnectionInfo.ts
@@ -1,15 +1,18 @@
 import { RTCPeerConnection } from 'wrtc';
 import { Socket } from 'socket.io';
+import { ClientStatus } from '../constants/client-status.constant';
 
 export class ClientConnectionInfo {
   private readonly _RTCPC: RTCPeerConnection;
   private _enterSocket: Socket | null;
   private _lectureSocket: Socket | null;
+  private _status: ClientStatus;
 
   constructor(RTCPC: RTCPeerConnection, enterSocket?: Socket) {
     this._RTCPC = RTCPC;
     this._enterSocket = enterSocket ?? null;
     this._lectureSocket = null;
+    this._status = ClientStatus.ONLINE;
   }
 
   get RTCPC(): RTCPeerConnection {
@@ -29,5 +32,9 @@ export class ClientConnectionInfo {
     this._enterSocket?.disconnect();
     this._lectureSocket?.leave(roomId);
     this._lectureSocket?.disconnect();
+  };
+
+  setOfflineStatus = () => {
+    this._status = ClientStatus.OFFLINE;
   };
 }
