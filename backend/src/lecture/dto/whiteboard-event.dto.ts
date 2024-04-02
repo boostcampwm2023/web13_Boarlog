f06e977f824f95f076e72162b3736111e1bd3ff3
tmddus2 <tmddus2__@naver.com>
2024-04-02 23:59:03 +0900

    fix: objects에 배열 값들만 number[] 배열로 저장되도록 수정

--- backend/src/lecture/dto/whiteboard-event.dto.ts
+++ backend/src/lecture/dto/whiteboard-event.dto.ts
@@ -10,12 +10,4 @@ export class WhiteboardEventDto {
   width: number;
 
   height: number;
-
-  constructor(log: WhiteboardLog) {
-    this.objects = new Uint8Array(log.objects);
-    this.viewport = log.viewport;
-    this.eventTime = log.eventTime;
-    this.width = log.width;
-    this.height = log.height;
-  }
 }
